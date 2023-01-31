ae05e2fae1bc7059ce27d6c9157d7e1cf512dc31
Vova <kandrin2002@mail.ru>
2023-01-31 22:13:06 +0100

    добавил генерацию изображений

--- src/data/api/openai_api.py
+++ src/data/api/openai_api.py
@@ -7,7 +7,7 @@ class OpenaiApi:
     def __init__(self, apiKey):
         openai.api_key = apiKey
 
-    def getAnswerToQuestion(self, question):
+    def completionCreate(self, question):
         result = openai.Completion.create(
             model="text-davinci-003",
             prompt=question,
@@ -17,4 +17,18 @@ class OpenaiApi:
             presence_penalty=0.0
         )
         return result.choices[0].text
+
+    def imageCreate(self, prompt):
+        result = openai.Image.create(
+            prompt=prompt,
+            n=5,
+            size="512x512",
+        )
+        result2 = result["data"]
+        images = []
+        for img in result2:
+            images.append(img["url"])
+
+        return images
+
 
